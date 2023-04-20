439263054170e888a260d4f0d3ee484ba3678ed5
elektroente <christler.jac@gmail.com>
2023-04-21 00:11:30 +0200

    allow dead code in files.rs

--- src/files.rs
+++ src/files.rs
@@ -1,3 +1,6 @@
+#[allow(dead_code)]
 const WINDOWS: &str = "%appdata%/Roaming/pico-8/appdata/picocad/";
+#[allow(dead_code)]
 const OSX: &str = "~/Library/Application Support/pico-8/appdata/picocad/";
+#[allow(dead_code)]
 const LINUX: &str = "~/.lexaloffle/pico-8/appdata/picocad/";
