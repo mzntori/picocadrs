68d866b2f0c8a203750a0e668242ac256ddbfb70
mzntori <mzntori@proton.me>
2024-04-13 01:09:45 +0200

    remove old code from src

--- src/old/files.rs
+++
@@ -1,6 +0,0 @@
-#[allow(dead_code)]
-const WINDOWS: &str = "%appdata%/Roaming/pico-8/appdata/picocad/";
-#[allow(dead_code)]
-const OSX: &str = "~/Library/Application Support/pico-8/appdata/picocad/";
-#[allow(dead_code)]
-const LINUX: &str = "~/.lexaloffle/pico-8/appdata/picocad/";
--- src/old/lib.rs
+++
@@ -1,9 +0,0 @@
-//! # Features
-//! picocadrs is a libary for working with [picoCAD] files.
-//! For that it has features to serialize and deserialize files.
-//!
-//! [picoCAD]: https://johanpeitz.itch.io/picocad
-
-pub mod assets;
-pub mod save;
-pub mod files;
